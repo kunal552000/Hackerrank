c7a8db9bff15a718c0b627786d88e73cda3bd4f2
kunal552000 <1906106@kiit.ac.in>
2020-08-11 13:22:43 +0530

    cpp code

---
+++ FIND MERGE POINT OF 2 LINKED LIST.cpp
@@ -0,0 +1,152 @@
+/*
+Given pointers to the head nodes of  linked lists that merge together at some point, find the Node where the two lists merge. It is guaranteed that the two head Nodes will be different, and neither will be NULL.
+
+In the diagram below, the two lists converge at Node x:
+
+[List #1] a--->b--->c
+                     \
+                      x--->y--->z--->NULL
+                     /
+     [List #2] p--->q
+Complete the int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) method so that it finds and returns the data value of the Node where the two lists merge.
+
+Input Format
+
+Do not read any input from stdin/console.
+
+The findMergeNode(SinglyLinkedListNode,SinglyLinkedListNode) method has two parameters,  and , which are the non-null head Nodes of two separate linked lists that are guaranteed to converge.
+
+Constraints
+
+The lists will merge.
+.
+ .
+
+Output Format
+
+Do not write any output to stdout/console.
+
+Each Node has a data field containing an integer. Return the integer data for the Node where the two lists merge.
+
+Sample Input
+
+The diagrams below are graphical representations of the lists that input Nodes  and  are connected to. Recall that this is a method-only challenge; the method only has initial visibility to those  Nodes and must explore the rest of the Nodes using some algorithm of your own design.
+
+Test Case 0
+
+ 1
+  \
+   2--->3--->NULL
+  /
+ 1
+Test Case 1
+
+1--->2
+      \
+       3--->Null
+      /
+     1
+Sample Output
+
+2
+3
+
+*/
+
+
+//CODE
+/*
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+*/
+
+// Complete the findMergeNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    struct SinglyLinkedListNode *pointer1,*pointer2;
+    pointer1 = head1;
+    pointer2 = head2;
+    if(pointer1 == NULL || pointer2 == NULL )
+    return NULL;
+    while(pointer1 != NULL && pointer2 != NULL && pointer1 != pointer2)
+    {
+        pointer1 = pointer1->next;
+        pointer2 = pointer2->next;
+        if(pointer1 == pointer2)
+           return pointer1->data;
+        if(pointer1 == NULL)
+        pointer1 = head2;
+        if(pointer2==NULL)
+        pointer2 = head1;
+    }
+    return pointer1->data;
+
+
+}
+
